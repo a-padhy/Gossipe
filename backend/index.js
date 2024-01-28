73b6a41eaf98588f3474dd5c8c7f09787bbc535b
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-28 19:46:32 +0530

    deploy server

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://gossipe.vercel.app",
   })
 );
 app.use(express.json());
@@ -36,7 +37,8 @@ const server = app.listen(port, (req, res) => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://gossipe.vercel.app",
+    // origin: "http://localhost:5173",
   },
 });
 
